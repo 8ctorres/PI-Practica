536e13f65f707626fb12c99e7e9ef6092d730e45
Carlos Torres <carlos.torres@udc.es>
2021-05-01 21:24:26 +0200

    Useful functions to retrieve data from the APIs and import it in DataFrames

---
+++ utils/aqicn.py
@@ -0,0 +1,55 @@
+"""
+    Código básico para ir extrayendo datos de AQICN y cargándolos en estructuras de Pandas
+    La idea es que luego esto lo copiemos ya dentro del proyecto de Django cuando tengamos
+    claro el sitio donde copiarlo
+
+"""
+
+import pandas as pd
+import numpy as np
+import requests as rq
+
+base_url = "https://api.waqi.info/"
+token = "b7818feb850f1306340bd0465824027131b20af8"
+
+# Obtener los datos de una o varias ciudades
+# Esta función acepta una lista de ciudades (strings)
+# y devuelve un DataFrame que contiene los datos de polución
+# del aire disponibles para esas ciudades
+
+def get_feed_ciudades(ciudades):
+    series_ciudades = []
+    for ciudad in ciudades:
+        resp = rq.get(base_url+"feed/"+ciudad+"/?token="+token)
+
+        if resp.json()['status'] != "ok":
+            raise Exception("API Request failed")
+
+        datos_ciudad = resp.json()['data']['iaqi']
+
+        series_ciudades.append(pd.Series(datos_ciudad).apply(lambda x: x['v']))
+
+    dataframe = pd.concat(series_ciudades, axis=1)
+    dataframe.columns = ciudades
+
+    return dataframe
+
+
+def get_datos_coords(lat=None, lon=None):
+    if lat is None or lon is None:
+        raise ValueError("No se indicaron coordenadas")
+
+    resp = rq.get(base_url+"feed/geo:"+str(lat)+";"+str(lon)+"/?token="+token)
+    jsondata = resp.json()
+
+    if jsondata['status'] != "ok":
+        raise Exception("API Request failed")
+
+    datos_loc = jsondata['data']['iaqi']
+    nombre_loc = jsondata['data']['city']['name']
+
+    serie = pd.Series(datos_loc).apply(lambda x: x['v'])
+    serie.name = nombre_loc
+
+    return serie
+
