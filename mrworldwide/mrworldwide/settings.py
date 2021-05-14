47defd0a6c0fea6610732f9a4736d4dcb271761b
Isma <ismael.verdec@udc.es>
2021-05-14 18:57:44 +0200

    Navbar añadida, mucha documentacion de bootstrap leida

--- mrworldwide/mrworldwide/settings.py
+++ mrworldwide/mrworldwide/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'homepage.apps.HomepageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #local apps
+    'homepage.apps.HomepageConfig',
 ]
 
 MIDDLEWARE = [
