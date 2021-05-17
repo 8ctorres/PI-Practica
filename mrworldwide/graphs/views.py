01fa612e1904650bc89d8973b3744f32fc032822
Isma <ismael.verdec@udc.es>
2021-05-17 13:35:57 +0200

    Vistas básicas para los CU relacionados con graficas creadas

--- mrworldwide/graphs/views.py
+++ mrworldwide/graphs/views.py
@@ -6,10 +6,10 @@ def graphs_index(request):
     return render(request, 'graphs/graph_index.html')
 
 def graphs_1dataXcountries(request):
-    return render(request, 'graphs/graph_index.html')
+    return render(request, 'graphs/graph_1dataXcountries.html')
 
 def graphs_2data1country(request):
-    return render(request, 'graphs/graph_index.html')
+    return render(request, 'graphs/graph_2data1country.html')
 
 def graphs_histogram(request):
-    return render(request, 'graphs/graph_index.html')
+    return render(request, 'graphs/graph_histogram.html')
