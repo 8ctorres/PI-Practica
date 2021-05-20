b042904db05f4602d1dbdf964eb63c26eb3b7e4e
itasahobby <itasahobby@gmail.com>
2021-05-20 17:25:18 -0400

    Created the views to process all needed methods

--- mrworldwide/user_management/views.py
+++ mrworldwide/user_management/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render,redirect
+from django.http import HttpResponseNotFound
+from flask import request
 
 # Create your views here.
 def login_view(request):
-	return render(request,'login.html')
+	if request.method == 'GET':
+		return render(request,'login.html')
+	elif request.method == 'POST':
+		return render(request,'login.html')
 
 def logout_view(request):
 	return redirect(request.GET['next'])
 
 def signup_view(request):
-	return render(request,'signup.html')
+	if request.method == 'GET':
+		return render(request,'signup.html')
+	if request.method == 'POST':
+		return redirect('index')
+
+def profile_view(request):
+	return render(request,'profile.html')
