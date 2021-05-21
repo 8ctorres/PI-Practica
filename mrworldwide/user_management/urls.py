e383d273e290eca0952d846f4d27e7f8c19c4adb
itasahobby <itasahobby@gmail.com>
2021-05-21 06:01:58 -0400

    Fixed error in urls pointing to wrong view

--- mrworldwide/user_management/urls.py
+++ mrworldwide/user_management/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-	url(r'^profile$', views.logout_view, name="profile"),
+	url(r'^profile$', views.profile_view, name="profile"),
     url(r'^login$', views.login_view, name="login"),
 	url(r'^logout$', views.logout_view, name="logout"),
-	url(r'^signup$', views.profile_view, name="signup"),
+	url(r'^signup$', views.signup_view, name="signup"),
 ]
