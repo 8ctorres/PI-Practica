d40f18b6ad735e4525179ac0af603befb4916455
itasahobby <itasahobby@gmail.com>
2021-05-23 20:37:31 +0200

    Added spain as default country to homepage

--- mrworldwide/homepage/views.py
+++ mrworldwide/homepage/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from search_countries.models import Pais
+from apis.restcountries import get_countries_by_name
 
 # Create your views here.
 def homepage(request):
-    return render(request, 'homepage/home.html')
+    country_df = get_countries_by_name("Spain").iloc[0]
+    country = country_df.to_dict()
+    context = {"country": country}
+    return render(request, 'homepage/home.html',context)
