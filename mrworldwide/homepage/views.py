d923076e5bdf5a95d60d691a7e6bb30e0a5d8a96
Carlos Torres <carlos.torres@udc.es>
2021-05-23 21:34:07 +0200

    Reduciendo código repetido

--- mrworldwide/homepage/views.py
+++ mrworldwide/homepage/views.py
@@ -1,10 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from apis.restcountries import get_countries_by_name
 
 # Create your views here.
 def homepage(request):
-    country_df = get_countries_by_name("Spain").iloc[0]
-    country = country_df.to_dict()
-    context = {"country": country}
-    return render(request, 'homepage/home.html',context)
+    return redirect("search") #TODO pasar el parámetro
