3aebf9b5608e4445b08a6cc762241ff074c70490
jusepe <jusepe@nix.nix.local>
2021-06-01 19:22:43 +0200

    Fixed topn HTML form and minor changes

--- mrworldwide/top_countries/views.py
+++ mrworldwide/top_countries/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def top(request):
-    return render(request, 'top_countries/top.html')
+    if request.method == 'GET':
+        return render(request, 'top_countries/top.html')
+    elif request.method == 'POST':
+        context = {'data': 123}
+        return render(request, 'top_countries/top.html', context)
