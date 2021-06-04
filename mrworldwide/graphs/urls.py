180e20733e58600f90939b6e70cacb2893b69e78
Isma <ismael.verdec@udc.es>
2021-06-04 13:06:35 +0200

    Navegabilidad completa entre pagina con el formulario y la pagina con el resultado para todos los graficos

--- mrworldwide/graphs/urls.py
+++ mrworldwide/graphs/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^2data1countries', views.graphs_2data1country, name='2data1country_base'),
     url(r'^histogram', views.graphs_histogram, name='histogram_base'),
     url(r'^1dXc_result', views.graphs_1dataXcountries_result, name='1dataXcountries_result'),
-    url(r'^2d1c_result', views.graphs_2data1country_result, name='1dataXcountries_result'),
+    url(r'^2d1c_result', views.graphs_2data1country_result, name='2data1country_result'),
     url(r'^hist_result', views.graphs_histogram_result, name='histogram_result'),
     
 ]
