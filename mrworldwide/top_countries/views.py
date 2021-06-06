6fbb347290a92a8a798010d8adc71d620c717b4f
jusepe <jusepe@nix.nix.local>
2021-06-06 13:21:21 +0200

    Topn feature complete

--- mrworldwide/top_countries/views.py
+++ mrworldwide/top_countries/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import render
 from jsonschema import validate, ValidationError
 from .schemas import topn_schema
 from apis.worldbank import get_indicator_names,get_indicator_code
-from apis.graphs import top_n_indicador
+from apis.graphs import graph_topn
 from apis.restcountries import get_all_countries
 from apis.exceptions import APIRequestException
+import os
+from uuid import uuid4
+import base64
+from .exceptions import TopAmountExceeded
+import traceback
 
 def top(request):
+    indicators = get_indicator_names()
+    indicators_names = [indicators[indicator] for indicator in indicators]
     if request.method == 'GET':
-        indicators = get_indicator_names()
-        indicators_names = [indicators[indicator] for indicator in indicators]
-
         context = {"indicators": indicators_names}
         return render(request, 'top_countries/top.html',context)
     elif request.method == 'POST':
@@ -20,14 +24,27 @@ def top(request):
             queryDict = request.POST.dict()
             unpackedBody = [ queryDict[key] for key in ["indicator","top"] ]
             indicator,top = unpackedBody
+            top = int(top)
             indicator_code = get_indicator_code(indicator)
-            serietop = gp.top_n_indicador(indicator_code,5)
-            print(serietop)
-            context = {'indicator': indicator, 'top': top}
+            if top > 50:
+                raise TopAmountExceeded(top)
+            graph_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), f"{uuid4()}.jpg")            
+            graph = graph_topn(indicator_code,top,graph_path)
+            with open(graph_path, "rb") as f:
+                content = f.read()
+                encoded_img = base64.b64encode(content).decode(encoding="utf-8")
+                os.remove(f.name)
+            context = {'indicator': indicator, 'top': top, 'graph': encoded_img}
         except ValidationError:
-            context = {'error': 'Invalid parameters'}
+            context = {"indicators": indicators_names,'error': 'Invalid parameters'}
+        except TopAmountExceeded:
+            context = {"indicators": indicators_names,'error': f'Maximum amount of countries allowed are 50, {top} requested'}
         except APIRequestException:
-            context = {'error': 'Server error'}
+            traceback.print_exc()
+            context = {"indicators": indicators_names,'error': 'Server error'}
+        except KeyError:
+            context = {"indicators": indicators_names,'error': 'Unsupported indicator'}
         except Exception:
-            context = {'error': 'Unexpected error'}
+            print(traceback.print_exc())
+            context = {"indicators": indicators_names,'error': 'Unexpected error'}
         return render(request, 'top_countries/top.html', context)
