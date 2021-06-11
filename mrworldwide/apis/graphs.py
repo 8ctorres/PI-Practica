795b9bed32a1aca3aaabc8370b27d888feb52a74
Carlos Torres <carlos.torres@udc.es>
2021-06-11 18:56:00 +0200

    Ajustados tamaños de las gráficas

--- mrworldwide/apis/graphs.py
+++ mrworldwide/apis/graphs.py
@@ -42,7 +42,7 @@ def graph_topn(ind, n=10, filename=None):
     serietop = top_n_indicador(ind, n)
 
     # Construyo el gráfico
-    graf = serietop.plot.bar()
+    graf = serietop.plot.bar(figsize=(10,8))
 
     # Si es el caso, lo guardo
     if filename is not None:
@@ -69,9 +69,9 @@ def graph_comparacion(ind, pais1, pais2, filename=None, tipo="l"):
     df = pd.concat([ind1, ind2], axis=1)
 
     if tipo=="d":
-        df.plot.scatter(x=ind1.name, y=ind1.name)
+        df.plot.scatter(x=ind1.name, y=ind1.name, figsize=(10,8))
     elif tipo=="l":
-        df.plot()
+        df.plot(figsize=(10,8))
     else:
         raise TypeError("Unknown type: "+tipo)
 
@@ -87,13 +87,13 @@ def graph_1dataXcountries(ind, paises, filename=None):
     series = []
 
     for ind in data:
-        serie = ind.value
+        serie = ind.value.dropna()
         serie.name = ind.countryName.iloc[0]
         series.append(serie)
 
     df = pd.concat(series, axis=1)
 
-    df.plot()
+    df.plot(figsize=(10,8))
 
     # Guardamos
     if filename is not None:
@@ -107,14 +107,14 @@ def graph_Xdata1country(inds, pais, filename=None):
     series = []
 
     for ind in data:
-        serie = ind.value
+        serie = ind.value.dropna()
         serie.name = ind.indicatorName.iloc[0]
         series.append(serie)
 
     df = pd.concat(series, axis=1)
 
     #TODO: Mirar el tema de hacerlos en varias escalas para evitar que uno opaque a los otros
-    df.plot()
+    df.plot(figsize=(10,8))
 
     if filename is not None:
         plt.savefig(filename)
@@ -122,7 +122,7 @@ def graph_Xdata1country(inds, pais, filename=None):
 
 def graph_histograma(ind, filename=None):
     indglobal = get_ind_global(ind)
-    indglobal.plot.hist()
+    indglobal.plot.hist(figsize=(10,8), bins=16)
 
     if filename is not None:
         plt.savefig(filename)
