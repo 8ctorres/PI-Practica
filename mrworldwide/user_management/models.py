78b95a8d4bf43efe872df6625fc0c96383293593
Jose <itasahobby@gmail.com>
2021-06-13 17:05:36 +0200

    Updated profile models

--- mrworldwide/user_management/models.py
+++ mrworldwide/user_management/models.py
@@ -2,15 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from jsonfield import JSONField
+from datetime import datetime
 
 class SingleIndicatorChart(models.Model):
+    date = models.DateTimeField(default=datetime.now, blank=True)
     indicator = models.CharField(default="Life expectancy", null=False, max_length=100)
-    image = models.CharField()
+    image = models.CharField(null=True,max_length=99999)
 
 class MultipleIndicatorChart(models.Model):
+    date = models.DateTimeField(default=datetime.now, blank=True)
     country = models.CharField(default="Spain", null=False, max_length=100)
-    image = models.CharField()
+    image = models.CharField(null=True,max_length=99999)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
