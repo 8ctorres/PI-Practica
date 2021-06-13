05d88d4ef0e3efb968dfcc1a6574332d086ff032
Carlos Torres <carlos.torres@udc.es>
2021-06-13 16:31:41 +0200

    Los metodos se tienen que llamar testXXX

--- mrworldwide/apis/test/test_worldbank.py
+++ mrworldwide/apis/test/test_worldbank.py
@@ -4,18 +4,15 @@ from apis import worldbank as wb
 
 class WorldBankTest(TestCase):
 
-    def setUp(self):
-        self.alltopics = wb.get_topics_list()
-
-
     # Test consistente en comprobar que los temas se sacan correctamente
-    def get_topics_test(self):
+    def test_get_topics(self):
+        self.alltopics = wb.get_topics_list()
         self.assertEquals(self.alltopics.loc['4'].topicName, "Education")
         self.assertEquals(self.alltopics.loc['11'].topicName, "Poverty")
 
     # Test consistente en sacar un identificador de la API, para luego poder
     # hacer consultas en base a ese indicador
-    def get_indicator_test(self):
+    def test_get_indicator(self):
         education_indicators = wb.get_indicators_from_topic("4")
         self.illiteracy_ind = education_indicators.loc["UIS.LP.AG15T99"]
         self.assertEquals(self.illiteracy_ind.indicatorName,
@@ -23,13 +20,13 @@ class WorldBankTest(TestCase):
 
     # Test consistente en sacar el dato concreto de un país en un año concreto,
     # de un indicador concreto. Usamos un dato antiguo porque suponemos que no va a cambiar
-    def get_value_test(self):
-        spain_illiteracy = wb.get_indicator("esp", self.illiteracy_ind.name)
+    def test_get_value(self):
+        spain_illiteracy = wb.get_indicator("esp", "UIS.LP.AG15T99")
         self.assertEquals(spain_illiteracy.value.loc['2008'].values[0], 931368)
 
     # Test para probar la funcion que dado un codigo da la descripcion detallada de un indicador
-    def get_def_test(self):
-        realdef = wb.get_indicator_definition(self.illiteracy_ind.name)
+    def test_get_def(self):
+        realdef = wb.get_indicator_definition("UIS.LP.AG15T99")
         expecteddef = "Total number of adults over age 15 who cannot both read and write with understanding a short simple statement on their everyday life."
         self.assertEquals(realdef, expecteddef)
 
